f6236e473369dd93956c8d10e10e8b526156b151
Natallia1990 <natashka_3@mail.ru>
2025-09-27 17:30:12 +0300

    Четвертая домашка

---
+++ lesson_05/lesson05_task1.py
@@ -0,0 +1,11 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/classattr")
+
+blue_button = driver.find_element(By.CSS_SELECTOR, "button.btn.btn-primary")
+blue_button.click()
+sleep(10)
---
+++ lesson_05/lesson05_task3.py
@@ -0,0 +1,18 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+input_field = driver.find_element(By.TAG_NAME, "input")
+
+input_field.send_keys("Sky")
+sleep(1)
+input_field.clear()
+
+input_field.send_keys("Pro")
+
+driver.quit()
+
+sleep(10)
